bfc7fe5854e9276f85efa9deefd153b091c82b4b
Michal Kimle <kimle.michal@gmail.com>
2019-02-18 10:51:17 +0100

    Update Salmon 0.9.1 -> 0.12.0

--- install/software/salmon.sh
+++ install/software/salmon.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-SALMON_VERSION="0.9.1"
+SALMON_NAME="salmon"
+SALMON_VERSION="0.12.0"
 
 function install_salmon {
-  wget --no-verbose https://github.com/COMBINE-lab/salmon/releases/download/v${SALMON_VERSION}/Salmon-${SALMON_VERSION}_linux_x86_64.tar.gz -P "${TMP_DIR}"
-  tar -zxf "${TMP_DIR}/Salmon-${SALMON_VERSION}_linux_x86_64.tar.gz" -C "${INSTALL_DIR}"
-  echo "export PATH=\$PATH:${INSTALL_DIR}/Salmon-latest_linux_x86_64/bin" >> "$PATH_FILE"
+  wget --no-verbose https://github.com/COMBINE-lab/${SALMON_NAME}/releases/download/v${SALMON_VERSION}/${SALMON_NAME}-${SALMON_VERSION}_linux_x86_64.tar.gz -P "${TMP_DIR}"
+  tar -zxf "${TMP_DIR}/${SALMON_NAME}-${SALMON_VERSION}_linux_x86_64.tar.gz" -C "${INSTALL_DIR}"
+  echo "export PATH=\$PATH:${INSTALL_DIR}/${SALMON_NAME}-${SALMON_VERSION}_linux_x86_64/bin" >> "$PATH_FILE"
 }
