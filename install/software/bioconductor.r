97602c3d7e11b8b69c284c8550b075b52215a47d
Michal Kimle <kimle.michal@gmail.com>
2019-02-21 13:48:04 +0100

    Add R packages
    * rmarkdown
    * DT
    * tidyverse
    * pander
    * Seurat

--- install/software/bioconductor.r
+++ install/software/bioconductor.r
@@ -4,7 +4,7 @@ if (!requireNamespace("BiocManager"))
     install.packages("BiocManager")
 BiocManager::install()
 
-packages <- c("Biobase","BiocParallel","DESeq2","GOstats","GOsummaries","GenomicAlignments","GenomicFeatures","Heatplus","KEGG.db","PoiClaClu","RColorBrewer","ReportingTools","Rsamtools","affy","airway","annotate","arrayQualityMetrics","beadarray","biomaRt","dendextend","gdata","genefilter","goseq","gplots","gtools","hgu133plus2cdf","hgu133plus2probe","hgu133plus2.db","hwriter","illuminaHumanv3.db","lattice","limma","lumi","made4","oligo","org.Hs.eg.db","org.Mm.eg.db","org.Rn.eg.db","pd.rat230.2","pheatmap","preprocessCore","qvalue","rat2302.db","sva","vsn","xtable")
+packages <- c("Biobase","BiocParallel","DESeq2","DT","GOstats","GOsummaries","GenomicAlignments","GenomicFeatures","Heatplus","KEGG.db","PoiClaClu","RColorBrewer","ReportingTools","Rsamtools","Seurat","affy","airway","annotate","arrayQualityMetrics","beadarray","biomaRt","dendextend","gdata","genefilter","goseq","gplots","gtools","hgu133plus2cdf","hgu133plus2probe","hgu133plus2.db","hwriter","illuminaHumanv3.db","lattice","limma","lumi","made4","oligo","org.Hs.eg.db","org.Mm.eg.db","org.Rn.eg.db","pander","pd.rat230.2","pheatmap","preprocessCore","qvalue","rat2302.db","rmarkdown","sva","tidyverse","vsn","xtable")
 
 BiocManager::install(packages)
 
--- install/software/bioconductor.sh
+++ install/software/bioconductor.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 function install_bioconductor_dependencies {
-  apt-get install -y libssl-dev libmariadb-client-lgpl-dev
+  apt-get install -y libssl-dev libmariadb-client-lgpl-dev libhdf5-dev
 }
 
 function install_bioconductor {
